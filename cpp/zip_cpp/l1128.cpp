de59300b79c76e6116cd95d04d8adaf2783d4e17
root <199450722@qq.com>
2021-01-27 00:50:54 +0800

    AutoCommit 2021-01-27 00:50:54

---
+++ cpp/zip_cpp/l1128.cpp
@@ -0,0 +1,64 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+class Solution {
+public:
+    int numEquivDominoPairs1(vector<vector<int>>& dominoes) {
+        if(dominoes.size()==0)
+            return 0;
+        sort(dominoes.begin(), dominoes.end(), comp);
+        int i = 0;
+        int initval = min(dominoes[i][0]*10+dominoes[i][1], dominoes[i][1]*10+dominoes[i][0]);
+        int ret=0;
+        int samecnt = 0;
+        for(int i = 1;i<dominoes.size();++i)
+        {
+            int val= min(dominoes[i][0]*10+dominoes[i][1], dominoes[i][1]*10+dominoes[i][0]);
+            if(val == initval)
+                samecnt++;
+            else
+            {
+                ret += samecnt*(samecnt+1)/2;
+                samecnt = 0;
+            }
+            initval = val;
+        }
+        ret += samecnt*(samecnt+1)/2;
+        return ret;
+    }
+
+    static bool comp(const vector<int> &a, const vector<int> &b)
+    {
+        int vala = min(a[0]*10+a[1], a[1]*10+a[0]);
+        int valb = min(b[0]*10+b[1], b[1]*10+b[0]);
+        return vala<valb;
+    }
+
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        vector<int> num(100,0);
+        int ret = 0;
+        for(auto c: dominoes)
+        {
+            int val = min(c[0]*10+c[1], c[1]*10+c[0]);
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+};
+
+
+
+int main()
+{
+    Solution A;
+    vector<vector<int>> dominoes{{1,2}, {2,1}, {3,4}, {2,8}, {8,2}, {4,2}, {2,4}, {2,1}};
+    cout<<A.numEquivDominoPairs(dominoes)<<endl;
+    return 0;
+}
---
+++ cpp/zip_cpp/l989.cpp
@@ -0,0 +1,83 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+*/
+
+//Class Solution
+class Solution {
+public:
+    vector<int> addToArrayForm1(vector<int>& A, int K) {
+        reverse(A.begin(), A.end());
+        int idx = 0;
+        int add = 0;
+        show(A);
+        while(K!=0 || add!=0)
+        {
+            if(A.size()<=idx)
+                break;
+            A[idx] = add + A[idx] + K % 10;
+            add = A[idx] / 10;
+            K = K / 10;
+            A[idx] = A[idx] % 10;
+            idx ++;
+        }//K == 0  ||  A.size() == idx
+        show(A);
+        cout<<"K:"<<K<<" add:"<<add<<endl;
+        while(K!=0 || add!=0)
+        {
+            int p = K%10 + add;
+            A.push_back(p%10);
+            add = p / 10;
+            K = K/10;
+        }
+        show(A);
+        reverse(A.begin(), A.end());
+        return A;
+    }
+
+    vector<int> addToArrayForm(vector<int>& A, int K) {
+        reverse(A.begin(), A.end());
+        int idx = 0;
+        int add = 0;
+        bool flag = false;
+        show(A);
+        while(K!=0 || add!=0)
+        {
+            if(A.size()<=idx || flag)
+            {
+                flag = true;
+                cout<<"f(A.size()<=idx)"<<endl;
+                cout<<"K:"<<K<<" add:"<<add<<endl;
+                show(A);
+                int p = K%10 + add;
+                A.push_back(p%10);
+                add = p / 10;
+                K = K/10;
+                cout<<"K:"<<K<<" add:"<<add<<endl;
+                continue;
+            }
+            A[idx] = add + A[idx] + K % 10;
+            add = A[idx] / 10;
+            K = K / 10;
+            A[idx] = A[idx] % 10;
+            idx ++;
+        }
+        show(A);
+        reverse(A.begin(), A.end());
+        return A;
+    }
+};
+int main()
+{
+    Solution A;
+    vector<int>nums{0};
+    int k = 10000;
+    //vector<int>nums{0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+    show(nums);
+    show(A.addToArrayForm(nums, k));
+
+    return 0;
+}
