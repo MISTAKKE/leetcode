d3c3b7c2a84fec31e90c45bc0f14d5696bd9ecd9
root <199450722@qq.com>
2021-01-28 23:41:46 +0800

    AutoCommit 2021-01-28 23:41:46

---
+++ cpp/l952.cpp
@@ -0,0 +1,31 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+class Solution {
+public:
+    int largestComponentSize(vector<int>& A)
+    {
+        int i = 1;
+
+    }
+};
+
+
+
+
+
+
+int main()
+{
+    Solution A;
+
+
+    return 0;
+}
