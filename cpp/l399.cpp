d785b42b6665d507882ee2def1c901fd1db43267
root <199450722@qq.com>
2021-02-11 00:03:01 +0800

    AutoCommit 2021-02-11 00:03:01

---
+++ cpp/l399.cpp
@@ -0,0 +1,30 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+class Solution
+{
+public:
+    class Union
+    {
+
+    };
+    vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries)
+    {
+        vector<double> ret;
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
