77cca3b5f0e6782d624501bf82cc8525ca0f4364
root <199450722@qq.com>
2021-02-25 23:50:01 +0800

    AutoCommit 2021-02-25 23:50:01

--- cpp/zip_cpp/l767.cpp
+++ cpp/zip_cpp/l767.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 /*
 description:
-
+S 只包含小写字母并且长度在[1, 500]区间内。
 
 */
 
@@ -24,7 +24,25 @@ public:
         {
             vec[c - 'a'].second += 1;
         }
-
+        sort(vec.begin(), vec.end(), [](pair<char, int> a, pair<char, int> b) { return a.second > b.second; });
+        int size = vec[0].second;
+        if (size > (S.size() + S.size() % 2) / 2)
+        {
+            return "";
+        }
+        vector<string> ans(size, "");
+        int l = 0;
+        forp(i, 0, 26)
+        {
+            int &n = vec[i].second;
+            char &c = vec[i].first;
+            forp(j, 0, n)
+            {
+                ans[l++ % size].push_back(c);
+            }
+        }
+        forp(i, 0, ans.size())
+            ret += (ans[i] + " ");
         return ret;
     }
 };
@@ -32,6 +50,8 @@ public:
 int main()
 {
     Solution A;
+    string str = "aab";
+    cout << A.reorganizeString(str) << endl;
 
     return 0;
 }
---
+++ cpp/zip_cpp/l876.cpp
@@ -0,0 +1,37 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for(int i = 0; i<n;++i)
+        {
+            for(int j = 0; j<m; ++j)
+            {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
