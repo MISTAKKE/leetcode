e3d81b906dc9b35f5eb674c151e041199e7d1826
root <199450722@qq.com>
2021-03-01 00:03:01 +0800

    AutoCommit 2021-03-01 00:03:01

---
+++ cpp/zip_cpp/l82.cpp
@@ -0,0 +1,30 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *p = head;
+        while(p != NULL && p->next !=NULL)
+        {
+            
+        }
+    }
+};
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
--- tool/helper.cpp
+++ tool/helper.cpp
@@ -11,7 +11,13 @@
 #include <map>
 #include <unordered_map>
 using namespace std;
-
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 struct TreeNode {
     int val;
     TreeNode *left;
