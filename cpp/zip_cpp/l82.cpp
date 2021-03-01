8733862d05aa8cf2864c2962313c17b5a36d45b8
root <199450722@qq.com>
2021-03-01 23:50:01 +0800

    AutoCommit 2021-03-01 23:50:01

--- cpp/zip_cpp/l82.cpp
+++ cpp/zip_cpp/l82.cpp
@@ -3,28 +3,143 @@ using namespace std;
 
 /*
 description:
-
-
 */
 
 //Class Solution
-
+void show(ListNode *head)
+{
+    cout << "Linknode: ";
+    while (head != NULL)
+    {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
 class Solution
 {
 public:
+    ListNode *deleteDuplicates1(ListNode *head)
+    {
+        ListNode p(0);
+        p.next = head;
+        ListNode *root = NULL;
+        while (p.next != NULL) //   pp(p) -> head(2) -> 2 -> 3
+        {
+            int val = p.next->val;
+            bool remove = false;
+            while (p.next->next != NULL && p.next->next->val == val)
+            {
+                remove = true;
+                p.next = p.next->next;
+            }
+            if (remove)
+            {
+                p.next = p.next->next;
+            }
+            else
+            {
+                p = *p.next;
+                if (root == NULL)
+                {
+                    root = &p;
+                }
+            }
+        }
+        return root == NULL ? p.next : root;
+    }
+    ListNode *deleteDuplicates3(ListNode *head)
+    {
+        ListNode *p = new ListNode(0);
+        p->next = head;
+        head = p;
+        ListNode *left, *right; // left 是左边可能重复的  right是右边可能重复的    p(head)  2(left, right)   2   3   3   4   null
+        while (p->next)
+        {
+            left = p->next;
+            right = left;
+            while (right->next && right->next->val == left->val)
+                right = right->next;
+            if (left == right)
+                p = p->next;
+            else
+                p->next = right->next;
+        }
+        return head->next;
+    }
     ListNode *deleteDuplicates(ListNode *head)
     {
-        ListNode *p = head;
-        while(p != NULL && p->next !=NULL)
+        ListNode p(0); //error
+        p.next = head;
+        head = &p; //head指向p
+        ListNode *left, *right;
+        while (p.next != NULL)
         {
-            
+            left = p.next;
+            right = left;
+            while (right->next != NULL && left->val == right->next->val)
+            {
+                right = right->next;
+            }
+            if (left == right)
+            {
+                p = *(p.next);
+            }
+            else
+            {
+                p.next = right->next;
+            }
         }
+        show(&p);
+        return head->next;
+    }
+
+    ListNode *deleteDuplicates5(ListNode *head)
+    {
+        ListNode *p = new ListNode(0); //ok
+        p->next = head;
+        head = p; //head指向空间
+        ListNode *left, *right;
+        while (p->next != NULL)
+        {
+            left = p->next;
+            right = left;
+            while (right->next != NULL && left->val == right->next->val)
+            {
+                right = right->next;
+            }
+            if (left == right)
+            {
+                p = p->next;
+            }
+            else
+            {
+                p->next = right->next;
+            }
+        }
+        show(p);
+        return head->next;
     }
 };
 
 int main()
 {
     Solution A;
+    ListNode l1(0);
+    ListNode l2(0);
+    ListNode l3(1);
+    ListNode l4(1);
+    ListNode l5(2);
+    ListNode l6(2);
+    ListNode l7(3);
+    l1.next = &l2;
+    l2.next = &l3;
+    l3.next = &l4;
+    l4.next = &l5;
+    l5.next = &l6;
+    l6.next = &l7;
+    show(&l1);
+    show(A.deleteDuplicates(&l1));
 
     return 0;
 }
---
+++ cpp/zip_cpp/lmt_m3w1.cpp
@@ -0,0 +1,25 @@
+#include <map>
+#include <vector>
+#include <iostream>
+#include <queue>
+using namespace std;
+// 输入第一行仅包含三个正整数n,x,y，分别表示参赛的人数和晋级淘汰人数区间。(1<=n<=50000,1<=x,y<=n)
+// 输入第二行包含n个整数，中间用空格隔开，表示从1号选手到n号选手的成绩。(1<=|a_i|<=1000)
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> m;
+        q.push(m);
+    }
+    if (n < 2 * rangex || n > 2 * rangey)
+    {
+        cout << -1;
+        return 0;
+    }
+    -----x----------y---------
+
+    return 0;
+}
