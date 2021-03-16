3f79933c8713884eb7d1c140260499a3dfd8b789
root <199450722@qq.com>
2021-03-16 23:50:01 +0800

    AutoCommit 2021-03-16 23:50:01

---
+++ cpp/zip_cpp/l41.cpp
@@ -0,0 +1,39 @@
+#include "../tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+*/
+
+//Class Solution
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        for (auto val : nums) // 4 3 -1 2
+        {
+            while (val <= nums.size() && val >= 1 && val != nums[val - 1])
+            {
+                int tmp = nums[val - 1];
+                nums[val - 1] = val;
+                val = tmp;
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return nums.size() + 1;
+    }
+};
+
+int main()
+{
+    Solution A;
+    vector<int> vec{4, 3, 1, 2};
+    cout << A.firstMissingPositive(vec) << endl;
+
+    return 0;
+}
