4e4899081496d69c1c6e13524376affe137f407e
root <199450722@qq.com>
2021-03-20 23:50:01 +0800

    AutoCommit 2021-03-20 23:50:01

--- cpp/zip_cpp/ltest.cpp
+++ cpp/zip_cpp/ltest.cpp
@@ -9,33 +9,17 @@ description:
 
 //Class Solution
 
-class Solution
-{
-public:
-    vector<int>dp;
-    Solution()
-    {
-        dp.resize(1000);
-        dp[0]=1;
-        dp[1]=1;
-    }
-    int f(int n )
-    {
-        if(dp[n] !=0)
-            return dp[n];
-        dp[n] = f(n-1) + f(n-2);
-        return dp[n];
-    }
-};
+
+
+
 
 
 int main()
 {
-    Solution A;
-    for(int i = 0; i<100; ++i)
-    {
-        A.f(i);
-    }
+    int i = 0;
+    char c = 1;
+    int d = (int)c;
+    cout<<d<<endl;
 
     return 0;
 }
