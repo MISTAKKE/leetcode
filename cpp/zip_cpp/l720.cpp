c94e0db2613306cb7662e4506ff3301c7aff3d02
root <199450722@qq.com>
2021-03-30 00:03:02 +0800

    AutoCommit 2021-03-30 00:03:01

--- cpp/zip_cpp/l720.cpp
+++ cpp/zip_cpp/l720.cpp
@@ -14,30 +14,38 @@ public:
     string longestWord(vector<string> &words)
     {
         return "";
+        Trie t;
     }
     class Node
     {
+    public:
         char c;
         bool end;
         map<char, Node> mp;
+        Node(char _c = ' ')
+        {
+            c = _c;
+            end = false;
+        }
     };
     class Trie
     {
     public:
-        char c;
-        
-        Trie()
+        Node root;
+        void insert(string &res)
         {
-            c = 0;
-            end = false;
-        }
-        bool insert(string &res)
-        {
-
+            Node &node = root;
+            for (auto &c : res)
+            {
+                if (node.mp.find(c) != node.mp.end())
+                {
+                    root = node.mp[c];
+                }
+            }
         }
         bool exist(string &res)
         {
-
+            return false;
         }
     };
 };
