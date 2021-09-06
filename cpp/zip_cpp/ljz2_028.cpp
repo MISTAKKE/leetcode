d4938a0605203b6f296e7817cb9eb2314cc92ba4
root <199450722@qq.com>
2021-09-06 23:50:01 +0800

    AutoCommit 2021-09-06 23:50:01

---
+++ cpp/zip_cpp/ljz2_028.cpp
@@ -0,0 +1,42 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+
+class Solution {
+public:
+    Node* last;
+    Node* flatten(Node* head) {
+        last = head;
+        dfs(head->child);
+        dfs(head->next);
+        return head;
+    }
+    void dfs(Node *head)
+    {
+        if(head == NULL) return head;
+        dfs(head->child);
+        last->next = head;
+        head->prev = last;
+        head->child = NULL;
+        last = head;
+        dfs(head->next);
+    }
+};
+
+
+
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
