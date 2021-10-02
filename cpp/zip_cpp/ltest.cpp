65d77fff08e523f82f86bf223bbbabb64c47eaa9
root <199450722@qq.com>
2021-10-02 23:50:01 +0800

    AutoCommit 2021-10-02 23:50:01

--- cpp/zip_cpp/ltest.cpp
+++ cpp/zip_cpp/ltest.cpp
@@ -23,26 +23,12 @@ description:
 
 
 
-bool IsRefundFundSourceFreeze(int fund_source) {
-  static const std::set<int> fund_source_freeze{1, 2, 4, 8 ,16};
-  for(const auto &it : fund_source_freeze)
-  {
-      cout<<"it:"<<it<<endl;
-      if(it & fund_source)
-      {
-          return true;
-      }
-  }
-  return false;
-}
-
-
 
 int main()
 {
-    int kk = 32;
-    if(IsRefundFundSourceFreeze(kk))
-        cout<<"ddd"<<endl;
+    int kk = -1;
+    uint32_t dd = (uint32_t)(kk);
+        cout<<dd<<endl;
 
     return 0;
 }
