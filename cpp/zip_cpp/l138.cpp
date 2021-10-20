554053029c397654b5044ce8cd539f2a6733970b
root <199450722@qq.com>
2021-10-20 23:50:01 +0800

    AutoCommit 2021-10-20 23:50:01

---
+++ cpp/zip_cpp/l138.cpp
@@ -0,0 +1,28 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        vector<uint64_t> dest;
+        vector<int>val;
+    }
+};
+
+
+
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
