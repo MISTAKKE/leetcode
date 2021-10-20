c49e463f4c812633d62fedfe5cc81cd4fa95a3a3
root <199450722@qq.com>
2021-10-21 00:03:01 +0800

    AutoCommit 2021-10-21 00:03:01

--- cpp/zip_cpp/l138.cpp
+++ cpp/zip_cpp/l138.cpp
@@ -9,14 +9,54 @@ description:
 
 //Class Solution
 
-class Solution {
+class Node {
 public:
-    Node* copyRandomList(Node* head) {
-        vector<uint64_t> dest;
-        vector<int>val;
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
 
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL)
+            return NULL;
+        map<uint64_t, int> dest_mp;
+        vector<int>val;
+        vector<int>random;
+        Node* headbk = head;
+        for(int i = 0; head!=NULL;++i)
+        {
+            dest_mp[(char*)head] = i;
+            val.push_back(head->val);
+            if(head->random == NULL)
+                random.push_back(0);
+            else
+                random.push_back(&(head->random));
+            head = head->next;
+        }
+        vector<Node>node;
+        for(int i = 0; i<val.size(); ++i)
+        {
+            node.push_back(new Node(val[i]));
+        }
+        for(int i = 0; i<val.size(); ++i)
+        {
+            if( i !=val.size()-1 )
+                node[i].next = node[i+1]; 
+            dest_mp[(char*)head] = i;
+            node[i].random = node[dest_mp[head->random]];
+        }
+        Node* ret = new Node(val[0]);
+        return ret;
+    }
+};
 
 
 
