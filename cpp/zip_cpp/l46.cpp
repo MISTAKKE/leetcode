6b6e1fb2cbb9947d9c08816f736f80d9962bb0e1
root <199450722@qq.com>
2021-11-07 23:50:02 +0800

    AutoCommit 2021-11-07 23:50:01

---
+++ cpp/zip_cpp/l46.cpp
@@ -0,0 +1,47 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        backtrack(res, nums, 0);
+        return res;
+    }
+    void backtrack(vector<vector<int>> &res, vector<int> &nums, int left)
+    {
+        if (left == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+        for (int i = left; i < nums.size(); ++i)
+        {
+            swap(nums[left], nums[i]);
+            backtrack(res, nums, left + 1);
+            swap(nums[left], nums[i]);
+        }
+    }
+};
+
+int main()
+{
+    Solution A;
+    vector<int> vec{1, 2, 3};
+    vector<vector<int>> res = A.permute(vec);
+    for (auto c : res)
+    {
+        show(c);
+    }
+
+    return 0;
+}
---
+++ cpp/zip_cpp/l47.cpp
@@ -0,0 +1,49 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+class Solution
+{
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> res;
+        backtrack(res, nums, 0);
+        return res;
+    }
+    void backtrack(vector<vector<int>> &res, vector<int> &nums, int left)
+    {
+        if (left == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+        set<int> processed;
+        for (int i = left; i < nums.size(); ++i)
+        {
+            if (processed.find(nums[i]) == processed.end())
+            {
+                processed.insert(nums[i]);
+                swap(nums[left], nums[i]);
+                backtrack(res, nums, left + 1);
+                swap(nums[left], nums[i]);
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution A;
+    vector<int> vec{1, 1, 2};
+    vector<vector<int>> res = A.permuteUnique(vec);
+    for (auto c : res)
+        show(c);
+    return 0;
+}
