69725ebb2a96737ff66f5fedb0ac5b7ed12f26f7
root <199450722@qq.com>
2021-11-19 00:03:01 +0800

    AutoCommit 2021-11-19 00:03:01

--- cpp/zip_cpp/l1148.cpp
+++ cpp/zip_cpp/l1148.cpp
@@ -15,50 +15,47 @@ public:
     vector<int> avoidFlood(vector<int> &rains)
     {
         vector<int> out;       //输出的操作序列
-        map<int, bool> source; //代表了当前水库是否是空的  source[k]=true 水库k是满的   否则就是空的
+        set<int> full;
         int idx = 0;
         for (int i = 0; i < rains.size(); ++i)
         {
             if (rains[i] == 0) //当天晴天，准备排水
             {
-                // cout << "第" << i << "天，今天是晴天 idx:" << idx << endl;
+                cout << "第" << i << "天，今天是晴天 idx:" << idx << endl;
                 //找到下个雨天，且把它值为空
                 while (idx < rains.size())
                 {
-                    if (source.find(rains[idx]) != source.end() && source[rains[idx]] == true)
-                    {
+                    if(full.count(rains[idx])==1)
                         break; //找到第一个满了的水库天数idx  其水库为rains[idx]
-                    }
                     idx++;
                 }
                 if (idx != rains.size())
                 {
-                    source[rains[idx]] = false;
+                    full.erase(rains[idx]);
                     out.push_back(rains[idx]);
-                    // cout << "第" << i << "天，把" << rains[idx] << "的水排了 idx:" << idx << endl;
+                    cout << "第" << i << "天，把" << rains[idx] << "的水排了 idx:" << idx << endl;
                     ++idx;
                 }
                 else
                 {
                     out.push_back(1);
-                    // cout << "今天是晴天，但没活干，摸鱼" << endl;
+                    cout << "今天是晴天，但没活干，摸鱼" << endl;
                 }
             }
             else //当天雨天，准备放工
             {
-                // cout << "第" << i << "天，今天是雨天 idx:" << idx << endl;
-                if ((source.find(rains[i]) != source.end()) && (source[rains[i]] == true))
+                cout << "第" << i << "天，今天是雨天 idx:" << idx << endl;
+                if(full.count(rains[idx])==1)
                 {
-                    // cout << "i:" << i << " 把" << rains[i] << "的水库满上了， 房塌了" << endl;
+                    cout << "i:" << i << " 把" << rains[i] << "的水库满上了， 房塌了" << endl;
                     return vector<int>();
                 }
-                source[rains[i]] = true;
-                // cout << "第" << i << "天，把" << rains[i] << "的水库满上了" << endl;
+                full.insert(rains[i]);
+                cout << "第" << i << "天，把" << rains[i] << "的水库满上了" << endl;
                 out.push_back(-1);
                 idx = i + 1;
             }
         }
-
         return out;
     }
 };
@@ -66,7 +63,7 @@ public:
 int main()
 {
     Solution A;
-    vector<int> vec{1, 0, 2, 3, 0, 1, 2};
+    vector<int> vec{1,0,2,0};
     show(A.avoidFlood(vec));
     return 0;
 }
