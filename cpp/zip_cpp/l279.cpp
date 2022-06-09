d7d86566e558776a858b8bb545b9c88658832775
root <804447702@qq.com>
2022-06-10 00:03:01 +0800

    AutoCommit 2022-06-10 00:03:01

--- cpp/zip_cpp/l279.cpp
+++ cpp/zip_cpp/l279.cpp
@@ -7,26 +7,21 @@ description:
 
 */
 
-//Class Solution
-class Solution
-{
-public:
-    int numSquares(int n)
-    {
+// Class Solution
+class Solution {
+  public:
+    int numSquares(int n) {
         vector<int> dp(n + 1, 0);
         dp[1] = 1;
-        for (int i = 2; i <= n; ++i)
-        {
-            if (i * i <= n)
-            {
+        for (int i = 2; i <= n; ++i) {
+            if (i * i <= n) {
                 dp[i * i] = 1;
             }
             if (dp[i] == 1)
                 continue;
-            int minval = i; //init as multi 1
-            for (int k = 1; k*k < i; ++k)
-            {
-                minval = min(minval, dp[k*k] + dp[i - k*k]);
+            int minval = i;  // init as multi 1
+            for (int k = 1; k * k < i; ++k) {
+                minval = min(minval, 1 + dp[i - k * k]);
             }
             dp[i] = minval;
         }
@@ -34,11 +29,9 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     Solution A;
-    for (int i = 1; i < 20; ++i)
-    {
+    for (int i = 1; i < 20; ++i) {
         cout << "i:" << i << " cnt:" << A.numSquares(i) << endl;
     }
 
---
+++ cpp/zip_cpp/l403.cpp
@@ -0,0 +1,22 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+//Class Solution
+
+
+
+
+
+
+int main()
+{
+    Solution A;
+
+    return 0;
+}
