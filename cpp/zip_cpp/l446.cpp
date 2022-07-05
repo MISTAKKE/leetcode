29ec7c35e6f028e85a50eeb9a1d619ac6b375a25
root <804447702@qq.com>
2022-07-06 00:03:01 +0800

    AutoCommit 2022-07-06 00:03:01

--- cpp/zip_cpp/l446.cpp
+++ cpp/zip_cpp/l446.cpp
@@ -13,16 +13,18 @@ class Solution {
   public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         // mp[i][d] = k  以nums[i]结尾，d为公差，的子序列个数为k
-        vector<map<int, int>> mp(nums.size(), map<int, int>{});
+        vector<map<long long int, long long int>> mp(nums.size(), map<long long int, long long int>{});
         int res = 0;
         for (int i = 1; i < nums.size(); ++i) {
             for (int j = 0; j < i; ++j) {
-                long long int d = nums[i] - nums[j];
-                if (d < INT_MIN || d > INT_MAX) {
-                    continue;
-                }
+                long long int d = (long long int)nums[i] - (long long int)nums[j];
                 mp[i][d] += (mp[j][d] + 1);
-                res += (mp[j][d] + 1);
+                //在处理完 nums[i] 后，需要将nums[i]结尾公差为d的子序列统计累加
+            }
+        }
+        for (int i = 1; i < nums.size(); ++i) {
+            for (auto c : mp[i]) {
+                res += c.second;
             }
         }
 
@@ -34,10 +36,11 @@ int main() {
     Solution A;
     // vector<int> nums{2, 4, 6, 8, 10};  // 7
     // vector<int> nums{2, 4, 6, 8};  // 3
-    vector<int> nums{1, 2, 3, 4, 5, 6};  // 12
+    // vector<int> nums{1, 2, 3, 4, 5, 6};  // 12
     // vector<int> nums{7, 7, 7, 7, 7};  // 16
-    // vector<int> nums{7, 7, 7, 7};  // 5
+    vector<int> nums{7, 7, 7, 7};  // 5
     // vector<int> nums{7, 7, 7};  // 1
+    // vector<int> nums{0, 2000000000, -294967296};
     cout << A.numberOfArithmeticSlices(nums) << endl;
 
     return 0;
