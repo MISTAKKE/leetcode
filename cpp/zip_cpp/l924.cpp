2b3536ae243ec9eb593da885e990059fde2772f1
root <804447702@qq.com>
2022-07-15 23:50:01 +0800

    AutoCommit 2022-07-15 23:50:01

--- cpp/zip_cpp/l924.cpp
+++ cpp/zip_cpp/l924.cpp
@@ -18,34 +18,55 @@ class Solution {
         vector<set<int>> effect(n, set<int>{});
         vector<set<int>> reverse(n, set<int>{});
         for (int i = 0; i < initial.size(); ++i) {
-            init(graph, effect, reverse, initial[i]);
+            effect[initial[i]].insert(initial[i]);
+            reverse[initial[i]].insert(initial[i]);
+            dfs(graph, effect, reverse, initial[i], initial[i]);
         }
+
+        cout << "show graph" << endl;
+        for (int i = 0; i < graph.size(); ++i)
+            show(graph[i]);
+        cout << "show effect" << endl;
+        for (int i = 0; i < effect.size(); ++i)
+            show(effect[i]);
+        cout << "show reverse" << endl;
+        for (int i = 0; i < reverse.size(); ++i)
+            show(reverse[i]);
+
         for (int i = 0; i < initial.size(); ++i) {
+            if (reverse[initial[i]].size() != 1)
+                continue;
             int val = 0;
             for (auto it : effect[initial[i]]) {
                 if (reverse[it].size() == 1) {
                     val += 1;
                 }
             }
-            if (val >= maxval) {
-                minidx = i;
+            if (val > maxval) {
+                minidx = initial[i];
+                maxval = val;
+            }
+            else if (val == maxval) {
+                minidx = min(minidx, initial[i]);
             }
         }
         return minidx;
     }
-    void init(vector<vector<int>>& graph, vector<set<int>>& effect, vector<set<int>>& reverse, int i) {
-        for (int k = 0; k < graph.size(); ++k) {
-            if (graph[i][k] == 1 && effect.count(k) == 0) {
-                effect[i].insert(k);
-                reverse[k].insert(i);
+    void dfs(vector<vector<int>>& graph, vector<set<int>>& effect, vector<set<int>>& reverse, int i, int k) {
+        for (int j = 0; j < graph.size(); ++j) {
+            if (graph[k][j] == 1 && effect[i].count(j) == 0) {
+                effect[i].insert(j);
+                reverse[j].insert(i);
                 dfs(graph, effect, reverse, i, j);
             }
         }
     }
 };
-
 int main() {
     Solution A;
+    vector<vector<int>> graph{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+    vector<int> initial{0, 1, 2};
+    cout << A.minMalwareSpread(graph, initial) << endl;
 
     return 0;
 }
