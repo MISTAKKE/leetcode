1900f3dcf6c25de47749f9caf7fe247677624784
root <804447702@qq.com>
2022-08-19 00:03:01 +0800

    AutoCommit 2022-08-19 00:03:01

--- cpp/zip_cpp/l162.cpp
+++ cpp/zip_cpp/l162.cpp
@@ -14,34 +14,49 @@ description:
 class Solution {
   public:
     int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] > nums[mid+1]){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return right;
+    }
+};
+
+class Solution2 {
+  public:
+    int findPeakElement(vector<int>& nums) {
         if (nums.size() == 1){
             return 0;
         }
         int left = 0, right = nums.size() - 1;
-        while (left <= right) {
+        while (1) {
             int mid = (left + right) / 2;
-            if(mid > left){
+            if(mid != left){
                 if(nums[mid] > nums[mid-1]){
-                    if(nums[mid] > nums[mid+1]){
-                        return mid;
-                    }
                     left = mid;
                 }
-                right = mid;
+                else{
+                    right = mid;
+                }
             }
             else if (mid == left){
-                //等于则说明还有1个或两个
-                if(left == right){
-                    return left;
-                }
-                return mid + 1;
+                //等于则说明还有1个或2个
+                return nums[left] > nums[right] ? left : right;
             }
         }
-        return left; //never catch
+        return 0; //never catch
     }
 };
 int main() {
     Solution A;
+    vector<int> nums{1,2,3,4,5,6,78};
+    cout<<A.findPeakElement(nums)<<endl;
 
     return 0;
 }
