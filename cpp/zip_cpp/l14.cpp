139d51b2f2ded804d1ea9307f3526d3ff1f65ab2
root <804447702@qq.com>
2022-08-19 23:50:01 +0800

    AutoCommit 2022-08-19 23:50:01

---
+++ cpp/zip_cpp/l14.cpp
@@ -0,0 +1,40 @@
+#include "/root/leetcode/tool/helper.cpp"
+using namespace std;
+
+/*
+description:
+
+
+*/
+
+// Class Solution
+
+class Solution {
+public:
+    class Node {                                                                                                                                                                                              
+      public:
+        int x;
+        int y;
+        int val;
+        Node(int x_, int y_, int val_) : x(x_), y(y_), val(val_){};
+        bool operator<(const Node& b) const {
+            return val > b.val;
+        }   
+    };  
+    static bool comp(const vector<int> &a, const vector<int> &b){
+        return a[0]< b[0];
+    }
+    
+    string demo(string s, vector<vector<int>>& pairs) {
+        vector<vector<int>>res;
+        sort(res.begin(), res.end(), comp);
+        priority_queue<Node, vector<Node>, less<Node>> neighbor;
+        return "";
+    }
+};
+
+int main() {
+    Solution A;
+
+    return 0;
+}
