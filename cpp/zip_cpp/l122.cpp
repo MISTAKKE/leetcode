d364921731754fdbd5482a3a5f074849facc5284
root <804447702@qq.com>
2022-08-22 00:03:01 +0800

    AutoCommit 2022-08-22 00:03:01

--- cpp/zip_cpp/l122.cpp
+++ cpp/zip_cpp/l122.cpp
@@ -6,8 +6,23 @@ description:
 
 
 */
-
 class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<=1){
+            return 0;
+        }
+        int hold = 0 - prices[0];
+        int not = 0;
+        for(int i = 1; i<prices.size(); ++i){
+            hold = max(not - prices[i], hold);
+            not = max(prices[i] + hold, not);
+        }
+        return not;
+    }
+};
+
+class Solution2 {
   public:
     int maxProfit(vector<int>& prices) {
         int hold = -1 * prices[0];
@@ -23,6 +38,7 @@ class Solution {
 
 int main() {
     Solution A;
-
+    vector<int> nums{7,1,5,3,6,4};
+    cout<<A.maxProfit(nums)<<endl;
     return 0;
 }
